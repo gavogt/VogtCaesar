93b204f1aa9b63ff07deaf5e6456aeb94ac83b36
gavogt <gvogt@asu.edu>
2018-08-25 09:32:51 -0700

    Entered more Displays

--- Practice_Caesar2/Program.cs
+++ Practice_Caesar2/Program.cs
@@ -10,18 +10,31 @@ namespace Practice_Caesar2
     {
         static void Main(string[] args)
         {
-            DisplayWelcome();
-
             // Variables 
             string message = default;
             int shift = default;
+            int optionSelection = default;
+
+            DisplayWelcome();
+            DisplayOptions();
+
+            // Bad design?
+            try
+            {
+                optionSelection = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                DisplayError();
+            }
 
             // Console color change
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
 
-            Console.WriteLine("Please enter your message to be encrypted: ");
+            Console.WriteLine("\nPlease enter your message to be encrypted: ");
             message = Console.ReadLine();
 
+            // Bad design?
             try
             {
                 Console.WriteLine("Please enter the shift amount: ");
@@ -29,9 +42,7 @@ namespace Practice_Caesar2
             }
             catch
             {
-                // Error message color change
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Error! Please try again");
+                DisplayError();
             }
         }
 
@@ -79,6 +90,33 @@ namespace Practice_Caesar2
         public static void DisplayWelcome()
         {
             Console.WriteLine("**********************"+"\n* Welcome to Caesar! *"+ "\n**********************");
+
+        }
+        #endregion
+
+        #region DisplayOptions
+        /// <summary>
+        /// Display options for the user to encrypt, decrypt or exit
+        /// </summary>
+        public static void DisplayOptions()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("1. For Encrypting your message");
+            Console.WriteLine("2. For Decrypting your message");
+            Console.WriteLine("3. To exit the system");
+
+        }
+        #endregion
+
+        #region ErrorDisplay
+        /// <summary>
+        /// Display an error message with a color of DarkRed
+        /// </summary>
+        public static void DisplayError()
+        {
+            // Error message with a color change!
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Error! Please try again");
 
         }
         #endregion
